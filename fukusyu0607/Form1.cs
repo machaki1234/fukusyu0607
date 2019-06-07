8c2be54d9b019e56a0ca417c66fae759acc9a1dc
retnton <38481077+retnton@users.noreply.github.com>
2019-06-07 13:51:06 +0900

    復習（自力）

--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -12,9 +12,46 @@ namespace fukusyu0607
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -15;
+        Point cpos = MousePosition;
+        
+
         public Form1()
         {
             InitializeComponent();
+            cpos = PointToClient(cpos);
+            Text = cpos.X + ", " + cpos.Y;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if(label1.Left<0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Top<0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+            
+
         }
     }
 }
